a9a168f376665fb058047fffcbacfff9d872843e
Jules Finck <55241502+copsfuraxx@users.noreply.github.com>
2025-06-05 12:55:51 +0200

    feat: pixel art

---
+++ init_pixel_art.gd
@@ -0,0 +1,13 @@
+@tool
+extends EditorScript
+
+
+func _run() -> void:
+	pass
+	ProjectSettings.set_setting("display/window/size/viewport_width", 640)
+	ProjectSettings.set_setting("display/window/size/viewport_height", 360)
+	ProjectSettings.set_setting("display/window/size/window_width_override", 1152)
+	ProjectSettings.set_setting("display/window/size/window_height_override", 648)
+	ProjectSettings.set_setting("display/window/stretch/mode", "viewport")
+	ProjectSettings.set_setting("rendering/textures/canvas_textures/default_texture_filter", 0)
+	ProjectSettings.save()
